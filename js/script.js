f1ab642957fbccbf3f85884e122c4a57fbff2be2
Ainul Ayu <dntschr@gmail.com>
2024-06-06 19:43:11 +0700

    feature js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-// ini javascript 
+function replaceName() {
+    let yourname = prompt("Hello ARMY, may I know your name?", "")
+    document.getElementById("yourname").innerHTML = yourname 
+} 
+replaceName();
+
+function formValidation(){
+    let nameInput = document.getElementById('name-input').value;
+    if(nameInput == null || nameInput == ""){
+        alert("Field cannot be blank");
+    } else{
+    console.log(nameInput);
+    }
+}
